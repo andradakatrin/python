16dbd404525e35fd839b644a35817c954833fef0
Pop Andrada <popndradakatrin97@gmail.com>
2023-02-27 11:25:41 +0200

    Curs_02-DataStructures

---
+++ Tema1.py
@@ -0,0 +1,19 @@
+my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+
+my_list.sort()
+print(my_list)
+
+my_list.reverse()
+print(my_list)
+
+my_list.sort()
+print(my_list)
+print(my_list[1::2])
+
+my_list.sort()
+print(my_list)
+print(my_list[0::2])
+
+my_list.sort()
+print(my_list)
+print(my_list[2::3])
---
+++ main.py
@@ -0,0 +1,38 @@
+# intrebari examen grila:
+# - "==" si "is"
+# - orice obiect in python are in spate un # care este la fel
+#
+# - structuri mutable (poate fi modificata: liste) / unmutable: set, tupluri
+# my_list
+# functie: 'list'
+# functie: 'lenght'
+#
+# 'slice' - copie a unei liste (shallow copy if my_list)
+# my_slice = my_numbers[::]
+# my_slice = my_numbers[::-1] - reversed order of list
+#
+# my_numbers = append('g')
+# .append() - adauga elemente
+#
+# Tupluri - structura unmutable
+# my_tuple = ()
+#
+# - transformam tuplu in lista - modific lista - dupa transform lista in tuplu !!
+#
+# Dictionar
+# {acolade goale} = diictionar jol
+# .key() - gasim cheile din dictionar
+# .values() - valorile din dictionar
+# .items() - returneaza tupluri
+#
+# Operatori de aprtenenta
+# .in - True / Flase
+#
+# Seturi
+# set {} - structura unmutable: elementele sunt unmutable/ setul se poate modifica
+# - contine elemente unice
+# - structura de data neordonata si neindexata
+# - elementele din seturi sunt unice
+#
+# metoda .pop () - scoate ultimul element din lista
+# metoda .pop () - scoate primul element din set
